720874a12f427ab6bac00027235c0ed9026d676f
varshitha <varshithakotla858@gmail.com>
2026-05-30 14:24:20 +0530

    javascript module first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("welcome to the community portal");
+
+window.onload=function(){
+    alert("page has been fully loaded");
+}
+
+const Eventname="community event";
+const date="2026-05-28";
+let seats=50;
+
+console.log(`events : ${Eventname}
+    date : ${Eventdate}
+    seats : ${Availableseats}`);
+
+    seats++;
+    console.log(`seats available before registration: ${seats}`);
+
+    seats--;
+     console.log(`seats available after registration: ${seats}`);
