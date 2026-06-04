084792f2253e4081259765afec6534c889f698f4
varshitha kotla <varshithakotla858@gmail.com>
2026-06-04 11:50:22 +0530

    Update FileReader to read from output.txt
    Fix file reading logic and handle exceptions properly.

--- java modules/FileReader.java
+++ java modules/FileReader.java
@@ -1,1 +1,20 @@
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+class FileReader{
+  public static void main(String[]args){
+    try{
+      File file=new File("output.txt");
+      System.out.println("file commits");
+      Scanner sc=new Scanner(System.in);
+      
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+catch(FileNotFoundException e){
+  Dystem.out.println("error message");
+}
+  }
+}
