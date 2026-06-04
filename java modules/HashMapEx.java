1c6d9a2d01674378615bca588af74bb05cb3517a
varshitha kotla <varshithakotla858@gmail.com>
2026-06-04 12:27:17 +0530

    Implement HashMap example for student records
    Added a HashMap example to store student IDs and names.

--- java modules/HashMapEx.java
+++ java modules/HashMapEx.java
@@ -1,1 +1,27 @@
 
+import java.util.*;
+
+
+public class HashMapEx{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        HashMap<Integer,String> map=new HashMap<>();
+        for(int i=0;i<3;i++){
+            System.out.println("enter student ids:");
+            int id=sc.nextInt();
+            sc.nextLine();
+            System.out.println("studnt names");
+            String name=sc.nextLine();
+            
+            map.put(id,name);
+
+
+        }
+        System.out.println("entered id");
+        int searchid=sc.nextInt();
+
+        System.out.println("searchedids are:"+map.get(searchid));
+        
+    }
+}
